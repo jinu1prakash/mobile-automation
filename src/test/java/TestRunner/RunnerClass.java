c34eabe3e8dc495a598cd1da3d90c02e4ca4f16f
jinu1prakash <87686597+jinu1prakash@users.noreply.github.com>
2022-06-20 12:47:41 +0100

    Runner Class Addition

---
+++ src/test/java/TestRunner/RunnerClass.java
@@ -0,0 +1,15 @@
+package TestRunner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/main/resources/feature-files",glue = "test.classes.runningservice.StepDefinition",tags = "@apkdemotest",plugin = {
+        "pretty",
+        "json:build/cucumber-report/cucumber.json",
+        "html:build/cucumber-report/cucumber.html",
+        "junit:build/cucumber-report/cucumber.xml"})
+public class RunnerClass {
+
+}
